22ff39bb832075747588cfeea275d3cf9c8c71a5
YurkaBest <Ivaschenko_u@ukr.net>
2021-11-14 16:44:06 +0200

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryRef = document.querySelector('.gallery');
+
+const markUp = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+    `;
+  })
+  .join('');
+galleryRef.insertAdjacentHTML('beforeend', markUp);
+
+// galleryRef.addEventListener("click", handerImgClick);
+
+new SimpleLightbox('.gallery a', {
+  captions: 'alt',
+  captionsDelay: 250,
+});
 
 console.log(galleryItems);
