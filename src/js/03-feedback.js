ef1a0ad686987d7fa84389fe473ce7587abd288c
YurkaBest <Ivaschenko_u@ukr.net>
2021-11-14 22:47:20 +0200

    3 not finish

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,23 @@
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input[name="email"]'),
+  message: document.querySelector('textarea[name="message"]'),
+};
+const dataSet = {};
 
+const handleTextInput = e => {
+  //   const message = e.tareget.message.value;
+  //   const email = e.target.email.value;
+  //   dataSet[email] = message;
+  //   localStorage.setItem('feedback-form-state', dataSet);
+  console.log(e.tareget);
+  //   console.log(dataSet);
+};
+
+const handleFormSubmit = e => {
+  e.preventDefault();
+};
+
+refs.form.addEventListener('submit', handleFormSubmit);
+refs.form.addEventListener('input', handleTextInput);
+// refs.email.addEventListener('input', handleTextInput);
