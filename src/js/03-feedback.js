90a7d3626e1aec1e06658b98951598d88c97ee87
YurkaBest <Ivaschenko_u@ukr.net>
2021-11-15 01:03:49 +0200

    3 not and

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,7 @@
+// import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+
 const refs = {
   form: document.querySelector('.feedback-form'),
   email: document.querySelector('input[name="email"]'),
@@ -6,18 +10,37 @@ const refs = {
 const dataSet = {};
 
 const handleTextInput = e => {
-  //   const message = e.tareget.message.value;
-  //   const email = e.target.email.value;
-  //   dataSet[email] = message;
-  //   localStorage.setItem('feedback-form-state', dataSet);
-  console.log(e.tareget);
+  if (e.target.name == refs.email.name) {
+    dataSet.email = e.target.value;
+    console.log(refs.email.value);
+  }
+  if (e.target.name == refs.message.name) {
+    dataSet.message = e.target.value;
+  }
+
+  const inputJSON = JSON.stringify(dataSet);
+
+  localStorage.setItem(STORAGE_KEY, inputJSON);
   //   console.log(dataSet);
 };
 
 const handleFormSubmit = e => {
   e.preventDefault();
+  e.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
 };
 
+populateText();
+
+function populateText() {
+  const savedMessadge = localStorage.getItem(STORAGE_KEY);
+  console.log(savedMessadge);
+  if (savedMessadge) {
+    refs.email.value = 5;
+    refs.message.value = 25;
+  }
+}
+
 refs.form.addEventListener('submit', handleFormSubmit);
+// refs.form.addEventListener('input', throttle(handleTextInput, 500));
 refs.form.addEventListener('input', handleTextInput);
-// refs.email.addEventListener('input', handleTextInput);
